acb5d84518a768a7c37c90b57a3b24066bb725d7
SONU74268 <shantilal3377@gmail.com>
2022-01-05 21:57:50 +0530

    create viewpage which takes names and return next page wtith hello

--- src/main/java/com/demo/webspring/controller/DemoController.java
+++ src/main/java/com/demo/webspring/controller/DemoController.java
@@ -3,21 +3,24 @@ package com.demo.webspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
 
-    @RequestMapping("/res")
-    public String sendRes(Model model) {
-        model.addAttribute("message", "Hello , Shanti Lal");
+    @RequestMapping("/")
+    public String sendIndex() {
         return "index";
     }
 
-    @RequestMapping("/demo")
-    @ResponseBody
-    public String setDemo() {
-        return "This is responce";
+    @RequestMapping("/showform")
+    public String showForm() {
+        return "showform";
     }
 
+    @RequestMapping("/processform")
+    public String processForm(@RequestParam("studentname") String studetname, Model model) {
+        model.addAttribute("sname", studetname);
+        return "processform";
+    }
 }
