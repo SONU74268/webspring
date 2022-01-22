ca2856af0ff947bb017198c3eda804c2ba755bca
SONU74268 <shantilal3377@gmail.com>
2022-01-22 21:58:33 +0530

    "updaing student Controller Data"

--- src/main/java/com/demo/webspring/controller/StudentController.java
+++ src/main/java/com/demo/webspring/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.demo.webspring.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.demo.webspring.model.Student;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class StudentController {
 
     @GetMapping("/student-registration")
-    public String showForm(Student student, Model model) {
+    public String showForm(Student student, Model model, BindingResult bindingResult) {
         List<String> list = Arrays.asList("Male", "Female", "other");
         model.addAttribute("sex", list);
         List<String> professions = Arrays.asList("Computer", "Civil", "Electrical");
@@ -24,7 +27,7 @@ public class StudentController {
     }
 
     @PostMapping("/student-registration")
-    public String studentReg(@ModelAttribute("student") Student student) {
+    public String studentReg(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
         System.out.println(student);
         return "student-succ";
     }
